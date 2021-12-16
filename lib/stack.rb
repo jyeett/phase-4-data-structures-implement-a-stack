48da71b0f6c2699ef38f9f54607a9b5abd78b32c
Jordan Yee <jordanyee799@gmail.com>
2021-12-16 14:31:17 -0800

    commit tree a5301c70518a0e81e1edb4b430888f227836714f

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,51 @@
 # your code here
+class Stack
+    def initialize(limit = -1)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @limit == -1
+            @stack.push(value)
+        elsif @stack.length >= @limit
+            raise "Stack Overflow"
+        else
+            @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop()
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        size == 0
+    end
+
+    def full?
+        size == @limit
+    end
+
+    def search(value)
+        i = @stack.length - 1
+        count = 0
+        while @stack[i] != value 
+            if i == 0
+                count = -1
+                break
+            end
+            count = count + 1
+            i = i - 1
+        end
+        count
+    end
+end
